a203e7ce68c363a328de0dfa2f283c06f2b88685
Victor Diaz <vjdiazt@unimagdalena.edu.co>
2026-06-27 21:36:00 -0500

    cache 2.0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,21 +11,5 @@ root.render(
   </React.StrictMode>,
 );
 
-// Register Service Worker for PWA / offline support
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => {
-        // eslint-disable-next-line no-console
-        console.log("NUMA SW registered:", reg.scope);
-      })
-      .catch((e) => {
-        // eslint-disable-next-line no-console
-        console.warn("NUMA SW registration failed:", e);
-      });
-  });
-}
-
 // Install background sync for queued submissions
 installSyncListeners();

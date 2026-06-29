8c50ff8b8a3b34af8171cce1e770ca6eb05380eb
Victor Diaz <vjdiazt@unimagdalena.edu.co>
2026-06-29 18:45:54 -0500

    estetica 3.4.2

--- frontend/src/pages/CourseDetail.jsx
+++ frontend/src/pages/CourseDetail.jsx
@@ -43,7 +43,7 @@ export default function CourseDetail() {
     }
     setOfflineFiles(fmap);
   };
-  useEffect(() => { load(); }, [id]);
+  useEffect(() => { load(); }, [id]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const downloadCourseOffline = async () => {
     setDownloading(true);
